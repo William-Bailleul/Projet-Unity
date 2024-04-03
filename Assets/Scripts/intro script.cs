c7d28c1300f55532e76bf473d521bc2731c7ac29
unknown <wbailleul@gamingcampus.local>
2024-04-03 16:16:26 +0200

    First commit, empty project and .gitignore

---
+++ Assets/Scripts/intro script.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Apagnan");
+
+        Debug.Log("Apagnan");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
