f6143dfe53a2c7f074e459022a29176d5ca38cb8
Loris Fiard <lorisfiard1@gmail.com>
2024-04-05 11:05:51 +0200

    decor

---
+++ Assets/Scripts/ColorSwitch.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorDetection : MonoBehaviour
+{
+
+    public string selectedColor = "none";
+    GameObject[] blueObjects;
+    GameObject[] greenObjects;
+    GameObject[] redObjects;
+
+
+
+    void ColorSwitch()
+    {
+        if (selectedColor == "blue")
+        {
+            foreach (GameObject obj in blueObjects)
+            {
+                obj.SetActive(true);
+            }
+
+        }
+        else
+        {
+            foreach (GameObject obj in blueObjects)
+            {
+                obj.SetActive(false);
+            }
+        }
+
+        if (selectedColor == "green")
+        {
+            foreach (GameObject obj in greenObjects)
+            {
+                obj.SetActive(true);
+            }
+
+        }
+        else
+        {
+            foreach (GameObject obj in greenObjects)
+            {
+                obj.SetActive(false);
+            }
+        }
+
+        if (selectedColor == "red")
+        {
+            foreach (GameObject obj in redObjects)
+            {
+                obj.SetActive(true);
+            }
+
+        }
+        else
+        {
+            foreach (GameObject obj in redObjects)
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+
+    void KeyDetect()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            selectedColor = "red";
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            selectedColor = "green";
+        }
+        if (Input.GetKeyDown(KeyCode.B)) {
+            selectedColor = "blue";
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blueObjects = GameObject.FindGameObjectsWithTag("Blue");
+        greenObjects = GameObject.FindGameObjectsWithTag("Green");
+        redObjects = GameObject.FindGameObjectsWithTag("Red");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyDetect();
+        ColorSwitch();
+
+    }
+}
--- Assets/Scripts/PlayerFeet.cs
+++ Assets/Scripts/PlayerFeet.cs
@@ -6,16 +6,25 @@ public class PlayerFeet : MonoBehaviour
 {
     public bool _isGrounded;
 
-
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
+        
+        // Vérifier si le rendu appartient au Sorting Layer cible
+        if (collision.gameObject.GetComponent<Renderer>() != null && collision.gameObject.GetComponent<Renderer>().sortingLayerName == "Ground")
+        {
             _isGrounded = true;
+        }
+
+        
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
+        
+        // Vérifier si le rendu appartient au Sorting Layer cible
+        if (collision.gameObject.GetComponent<Renderer>() != null && collision.gameObject.GetComponent<Renderer>().sortingLayerName == "Ground")
+        {
             _isGrounded = false;
+        }
     }
 }
