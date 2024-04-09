fb51a4b1b67e4eb4a14fed3f2727df0901fe6fb0
William-Bailleul <wbailleul@gaming.tech>
2024-04-09 13:08:35 +0200

    Coyote and Buffer + new scenes

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,17 +6,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private ScriptableStats _stats;
     public Rigidbody2D _rb;
     public PlayerFeet _feet;
 
     //Jump Parameter
-    public float timeLeftGrounded;
-    private float _timeLeftGrounded = float.MinValue;
+    public float _horizontal = 10f;
+    public float _jumpForce = 35f;
+    private float _time;
 
     private bool _isJumping;
     public bool _isFrozen;
 
+    //Coyote and Buffer
+    public static float _coyoteTime = .15f;
+    private float _coyotTimeCounter;
+    public static float _JumpBuffer = .2f;
+    private float _bufferTimeCounter;
 
 
     // Start is called before the first frame update
@@ -35,50 +40,50 @@ public class Player : MonoBehaviour
         if (!_isFrozen)
         {
             if (Input.GetKey(KeyCode.D))
-                currentVelocity.x += _stats.horizontal;
+                currentVelocity.x += _horizontal;
             if (Input.GetKey(KeyCode.A))
-                currentVelocity.x -= _stats.horizontal;
+                currentVelocity.x -= _horizontal;
         }
         _rb.velocity = currentVelocity;
+
+
 
         if (!_isFrozen)
         {
             //Jump event
-            if (Input.GetKeyDown(KeyCode.Space) && _feet._isGrounded)
+            if (_feet._isGrounded)
             {
-                onJumpEvent();
+                _coyotTimeCounter = _coyoteTime;
             }
-            if (Input.GetKeyDown(KeyCode.Space) && !_feet._isGrounded && _timeLeftGrounded + _stats.coyoteTime > Time.time)
+            else
             {
-                onJumpEvent();
+                _coyotTimeCounter -= Time.deltaTime;
             }
-            //Jump continuously while holding till limit
-            if (Input.GetKey(KeyCode.Space) && _isJumping)
-            {
-                _stats.currentJumpTime += Time.deltaTime;
 
-                if (_stats.currentJumpTime < _stats.maxTimeJump)
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _bufferTimeCounter = _JumpBuffer;
+
+                if (_coyotTimeCounter > 0f && _bufferTimeCounter > 0f)
                 {
-                    //print("jumping");
                     onJumpEvent();
+                    _bufferTimeCounter = 0f;
                 }
             }
             else
             {
-                _isJumping = false;
+                _bufferTimeCounter -= Time.deltaTime;
+
             }
 
-            if (_feet._isGrounded == true)
+
+            //Jump continuously while holding till limit
+            if (Input.GetKeyUp(KeyCode.Space) && _rb.velocity.y > 0f)
             {
-                _stats.currentJumpTime = 0;
-                _isJumping = false;
+                _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y * 0.25f);
+                _coyotTimeCounter = 0f;
             }
         }
-        
-    }
-
-    private void FixedUpdate()
-    {
         
     }
 
@@ -86,6 +91,6 @@ public class Player : MonoBehaviour
     {
         _feet._isGrounded = false;
         _isJumping = true;
-        _rb.velocity = new Vector2(_rb.velocity.x, _stats.jumpForce);
+        _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
     }
 }
